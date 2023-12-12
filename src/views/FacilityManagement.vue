c573e7d52efc6baf3c2a0a0379dffa8824b49aba
k2maan <ketuman333@gmail.com>
2023-12-12 15:49:25 +0530

    Implemented: statistics on home page (#88)

--- src/views/FacilityManagement.vue
+++ src/views/FacilityManagement.vue
@@ -13,12 +13,28 @@
             <ion-icon slot="start" :icon="businessOutline"/>
             <h1>{{ translate("Facilities") }}</h1>
           </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Stores') }}</ion-label>
+            <ion-note slot="end">{{ physicalStoreFacilitiesCount }}</ion-note>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Warehouses') }}</ion-label>
+            <ion-note slot="end">{{ warehouseFacilitiesCount }}</ion-note>
+          </ion-item>
         </ion-card>
 
         <ion-card button @click="router.push('/parking')">
           <ion-item lines="none">
             <ion-icon slot="start" :icon="golfOutline"/>
             <h1>{{ translate("Parking") }}</h1>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Brokering queue') }}</ion-label>
+            <ion-note slot="end">{{ brokeringQueueOrderCount }}</ion-note>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Backorder parking') }}</ion-label>
+            <ion-note slot="end">{{ backorderParkingOrderCount }}</ion-note>
           </ion-item>
         </ion-card>
 
@@ -27,12 +43,27 @@
             <ion-icon slot="start" :icon="albumsOutline"/>
             <h1>{{ translate("Groups") }}</h1>
           </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Pickup facilities') }}</ion-label>
+            <ion-note slot="end">{{ pickupFacilitiesCount }}</ion-note>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ translate('Online facilities') }}</ion-label>
+            <ion-note slot="end">{{ onlineFacilitiesCount }}</ion-note>
+          </ion-item>
         </ion-card>
 
         <ion-card button @click="router.push('/settings')">
           <ion-item lines="none">
             <ion-icon slot="start" :icon="settingsOutline"/>
             <h1>{{ translate("Settings") }}</h1>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ userProfile?.partyName }}</ion-label>
+            <ion-note slot="end">{{ userProfile?.userLoginId }}</ion-note>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ instanceUrl }}</ion-label>
           </ion-item>
         </ion-card>
       </main>
@@ -47,6 +78,8 @@ import {
   IonHeader,
   IonIcon,
   IonItem,
+  IonLabel,
+  IonNote,
   IonPage,
   IonTitle,
   IonToolbar
@@ -60,6 +93,10 @@ import {
 } from 'ionicons/icons';
 import { useRouter } from 'vue-router';
 import { translate } from '@hotwax/dxp-components'
+import { hasError } from '@/adapter';
+import logger from '@/logger';
+import { FacilityService } from '@/services/FacilityService';
+import { mapGetters } from 'vuex';
 
 export default defineComponent({
   name: 'FacilityManagement',
@@ -69,9 +106,91 @@ export default defineComponent({
     IonHeader,
     IonIcon,
     IonItem,
+    IonLabel,
+    IonNote,
     IonPage,
     IonTitle,
     IonToolbar
+  },
+  computed: {
+    ...mapGetters({
+      userProfile: 'user/getUserProfile',
+      instanceUrl: 'user/getInstanceUrl',
+    })
+  },
+  data() {
+    return {
+      warehouseFacilitiesCount: 0,
+      physicalStoreFacilitiesCount: 0,
+      brokeringQueueOrderCount: 0,
+      backorderParkingOrderCount: 0,
+      pickupFacilitiesCount: 0,
+      onlineFacilitiesCount: 0
+    }
+  },
+  async ionViewWillEnter() {
+    await this.fetchFacilityTypeCount()
+    await this.fetchParkingsOrderCount()
+    await this.fetchGroupsFacilitiesCount()
+  },
+  methods: {
+    async fetchFacilityTypeCount() {
+      const warehouseTypePayload = {
+        "inputFields": {
+          'parentFacilityTypeId': 'DISTRIBUTION_CENTER',
+        },
+        "entityName": "FacilityAndProductStore",
+        "distinct": "Y",
+        "fieldList": ['facilityId', 'facilityName', 'facilityTypeId'],
+        "viewSize": 1
+      }
+  
+      const physicalStoreTypePayload = {
+        "inputFields": {
+          'parentFacilityTypeId': 'PHYSICAL_STORE',
+        },
+        "entityName": "FacilityAndProductStore",
+        "distinct": "Y",
+        "fieldList": ['facilityId', 'facilityName', 'facilityTypeId'],
+        "viewSize": 1
+      }
+
+      try {
+        const responses = await Promise.allSettled([
+          FacilityService.fetchFacilities(warehouseTypePayload),
+          FacilityService.fetchFacilities(physicalStoreTypePayload)
+        ])
+        
+        const failedResponse = responses.find((response: any) => hasError(response.value) && !response?.data?.count)
+        if (failedResponse) {
+          throw failedResponse
+        }
+
+        const [warehouseFacilitiesResponse, physicalStoreFacilitiesResponse] = responses.map((response: any) => response.value);
+        this.warehouseFacilitiesCount = warehouseFacilitiesResponse.data.count
+        this.physicalStoreFacilitiesCount = physicalStoreFacilitiesResponse.data.count
+      } catch (error) {
+        logger.error('Failed to fetch facility types count', error)
+      }
+    },
+    async fetchParkingsOrderCount() {
+      try {
+        const parkingsOrderCount = await FacilityService.fetchOrderCountsByFacility(['_NA_', 'BACKORDER_PARKING'])
+        this.brokeringQueueOrderCount = parkingsOrderCount['_NA_'] || 0
+        this.backorderParkingOrderCount = parkingsOrderCount['BACKORDER_PARKING'] || 0
+      } catch (error) {
+        logger.error('Failed to fetch facility types count', error)
+      }
+    },
+    async fetchGroupsFacilitiesCount() {
+      try {
+        const groupsFacilityCount = await FacilityService.fetchFacilityCountByGroup(['PICKUP', 'OMS_FULFILLMENT'])
+        this.pickupFacilitiesCount = groupsFacilityCount['PICKUP'] || 0
+        this.onlineFacilitiesCount = groupsFacilityCount['OMS_FULFILLMENT'] || 0
+      } catch (error) {
+        logger.error('Failed to fetch facility types count', error)
+      }
+    },
   },
   setup() {
     const router = useRouter();
