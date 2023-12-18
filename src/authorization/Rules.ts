3820a69a79af3f9b6ba6c918a2e4dd22a22b3370
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-12-18 16:28:34 +0530

    Improved: permissionId for app access(#117)

--- src/authorization/Rules.ts
+++ src/authorization/Rules.ts
@@ -1,3 +1,3 @@
 export default {
-  "COMMON_ADMIN": "COMMON_ADMIN"  // TODO: update the permission check logic inside action, kept key as COMMON_ADMIN as the permission checking logic uses keys to check against the env value
+  "FACILITIES_APP_VIEW": "FACILITIES_APP_VIEW"
 } as any
