187dd8a086c4f83e2186e0926da0b1508347dcda
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-12-18 16:17:01 +0530

    Implemented: permission support in app on login(#117)

--- src/authorization/Actions.ts
+++ src/authorization/Actions.ts
@@ -1,2 +1,3 @@
 export default {
+  "COMMON_ADMIN": "COMMON_ADMIN"
 }
--- src/authorization/Rules.ts
+++ src/authorization/Rules.ts
@@ -1,2 +1,3 @@
 export default {
+  "COMMON_ADMIN": "COMMON_ADMIN"  // TODO: update the permission check logic inside action, kept key as COMMON_ADMIN as the permission checking logic uses keys to check against the env value
 } as any
