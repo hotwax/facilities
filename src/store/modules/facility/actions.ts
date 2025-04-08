ba54665f8e06807d0a614552b6a9248a3a3c9941
R-Sourabh <rsourabh002@gmail.com>
2025-04-08 15:58:30 +0530

    Improved: Removed unnecessary code from the facility actions file (#256)

--- src/store/modules/facility/actions.ts
+++ src/store/modules/facility/actions.ts
@@ -9,7 +9,7 @@ import * as types from './mutation-types'
 import logger from '@/logger'
 
 const actions: ActionTree<FacilityState, RootState> = {
-  async fetchFacilitiesAdditionalInformation({ commit, state, rootGetters }, payload = { viewIndex: 0 }) {
+  async fetchFacilitiesAdditionalInformation({ commit, state }, payload = { viewIndex: 0 }) {
 
     // getting all the facilties from state
     const cachedFacilities = JSON.parse(JSON.stringify(state.facilities.list)); // maintaining cachedFacilities as to prepare the facilities payload to fetch additional information
@@ -39,7 +39,6 @@ const actions: ActionTree<FacilityState, RootState> = {
       facility.orderCount = facilitiesOrderCount[facility.facilityId] ? facilitiesOrderCount[facility.facilityId] : 0;
 
       const facilityGroupInformation = facilitiesGroupInformation[facility.facilityId]
-      const inventoryGroups = rootGetters['util/getInventoryGroups'];
 
       if(facilityGroupInformation?.length) {
         facility.groupInformation = facilityGroupInformation
@@ -139,8 +138,8 @@ const actions: ActionTree<FacilityState, RootState> = {
     commit(types.FACILITY_LIST_UPDATED, { facilities, total: facilities.length })
   },
 
-  async fetchFacilityAdditionalInformation({ commit, state, rootGetters }, facilityInfo) {
-    const facility = facilityInfo ? facilityInfo : JSON.parse(JSON.stringify(state.current))
+  async fetchFacilityAdditionalInformation({ commit, state, rootGetters }) {
+    const facility = JSON.parse(JSON.stringify(state.current))
     const inventoryGroups = rootGetters['util/getInventoryGroups'];
     
     const [facilityGroupInformation, facilityOrderCount] = await Promise.all([FacilityService.fetchFacilityGroupInformation([facility.facilityId]), FacilityService.fetchFacilitiesOrderCount([facility.facilityId])])
