7a1e77a041d0aeba140f5c4e1e346601de2ceb5d
Yash Maheshwari <yashmaheshwari551@gmail.com>
2025-11-13 16:49:01 +0530

    Fixed: issue of staff name displayed incorrect on details page(#387)

--- src/store/modules/facility/actions.ts
+++ src/store/modules/facility/actions.ts
@@ -439,7 +439,7 @@ const actions: ActionTree<FacilityState, RootState> = {
         parties = resp.data.docs
 
         parties.map((party: any) => {
-          party.fullName = party.groupName ? party.groupName : `${party.firstName} ${party.lastName}`
+          party.fullName = party.groupName ? party.groupName : (party.firstName || party.lastName) ? `${party.firstName ? party.firstName : ""} ${party.lastName ? party.lastName : ""}` : party.partyId
         });
       } else {
         throw resp.data
